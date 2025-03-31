47e4507cf1530a9cfdbadb17951e89bef603e1f7
Leozin777 <leohuemaster@gmail.com>
2025-03-31 19:43:55 -0300

    ✨ feat: pokemon app feito

--- pokemonapp/app/src/main/java/com/example/pokemonapp/MainActivity.kt
+++ pokemonapp/app/src/main/java/com/example/pokemonapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.pokemonapp
-
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +11,22 @@ class MainActivity : AppCompatActivity() {
 
         //configs
         setContentView(R.layout.activity_main)
-
-        val meuTexto = findViewById<TextView>(R.id.texto)
         val btn = findViewById<Button>(R.id.button)
+        val imageView = findViewById<ImageView>(R.id.imageView)
 
+        getPhotoRandom(number = 1, imageView = imageView)
+
+        (1..200).random()
         btn.setOnClickListener {
-            meuTexto.text = "Leo"
+            val number = (1..200).random()
+            getPhotoRandom(number = number, imageView = imageView)
         }
+    }
+
+    private fun getPhotoRandom(number: Int, imageView: ImageView){
+        Glide
+            .with(this)
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png")
+            .into(imageView)
     }
 }
